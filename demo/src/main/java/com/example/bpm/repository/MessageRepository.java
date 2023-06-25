6bd88440b57a8397dca339391668f7f72dd926d7
ppark__jjong <101304928+Parkjonghyeok2@users.noreply.github.com>
2023-06-26 02:25:38 +0900

    first

---
+++ demo/src/main/java/com/example/bpm/repository/MessageRepository.java
@@ -0,0 +1,14 @@
+package com.example.bpm.repository;
+
+import com.example.bpm.dto.MessageDto;
+import com.example.bpm.entity.MessageEntity;
+import com.example.bpm.entity.ProjectRequestEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+    List<MessageEntity> findAllByUserIdToMessageRecv_Uuid(String recvUserUuid);
+
+    List<MessageEntity> findAllByUserIdToMessageSend_Uuid(String sendUserUuid);
+}
